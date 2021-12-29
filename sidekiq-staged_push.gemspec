75f7e98d17dad8f6950c5c62df861ec3524fb4fd
Adam Niedzielski <adamsunday@gmail.com>
2021-12-29 21:17:11 +0100

    Setup database in test environment

--- sidekiq-staged_push.gemspec
+++ sidekiq-staged_push.gemspec
@@ -32,6 +32,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency "activerecord", ">= 5.2.0"
   spec.add_dependency "sidekiq", ">= 6.3.1"
   spec.add_development_dependency "bundler"
+  spec.add_development_dependency "railties"
   spec.add_development_dependency "rake"
   spec.add_development_dependency "rspec"
   spec.add_development_dependency "rubocop"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,12 @@
 require "bundler/setup"
 require "sidekiq/staged_push"
 require "active_record"
+require "rails/generators"
 
 ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
+Rails::Generators.invoke("sidekiq:staged_push:install", ["--force"])
+migrations_path = File.expand_path("../db/migrate/", File.dirname(__FILE__))
+ActiveRecord::MigrationContext.new(migrations_path, ActiveRecord::SchemaMigration).migrate
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
