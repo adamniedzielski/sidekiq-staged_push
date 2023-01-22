af165a8b0ad9789795046ac4891d1eff49a4925a
Bob Mazanec <bobmazanec@users.noreply.github.com>
2023-01-22 16:03:07 -0600

    Prep for 6.5.0+ support
    Begin work on/toward adamniedzielski/sidekiq-staged_push#10
    
    Excluding 7.x so far to make sure it works w/ 6.5+

--- lib/sidekiq/staged_push.rb
+++ lib/sidekiq/staged_push.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require "sidekiq"
-require "sidekiq/worker"
+require "sidekiq/job"
 require "sidekiq/staged_push/enqueuer"
 require "sidekiq/staged_push/worker"
 require "sidekiq/staged_push/version"
