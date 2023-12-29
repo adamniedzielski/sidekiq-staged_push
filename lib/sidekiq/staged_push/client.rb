a5be8c6b382e42bfb1c70f7343bd947f9037e49b
Adam Niedzielski <adam@sundaycoding.com>
2023-12-29 17:17:58 +0100

    Use new interface introduced in Sidekiq 6.5 (#16)
    * Use new interface introduced in Sidekiq 6.5
    
    * Fix Rubocop

--- lib/sidekiq/staged_push/client.rb
+++ lib/sidekiq/staged_push/client.rb
@@ -1,12 +1,23 @@
 # frozen_string_literal: true
 
+require "sidekiq/client"
 require "sidekiq/staged_push/staged_job"
 
 module Sidekiq
   module StagedPush
     class Client
+      def initialize(pool: nil, config: nil)
+        @redis_client = Sidekiq::Client.new(pool: pool, config: config)
+      end
+
       def push(item)
         StagedJob.create!(payload: item)
+      end
+
+      # Same as for Sidekiq::TransactionAwareClient we don't provide
+      # transactionality for push_bulk.
+      def push_bulk(items)
+        @redis_client.push_bulk(items)
       end
     end
   end
