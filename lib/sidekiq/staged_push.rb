d7e2421f3374851afc532ef08f76979368910b8f
Adam Niedzielski <adamsunday@gmail.com>
2024-01-24 16:18:58 +0100

    Use Sidekiq::Component for Enqueuer
    Fixes #17

--- lib/sidekiq/staged_push.rb
+++ lib/sidekiq/staged_push.rb
@@ -12,9 +12,9 @@ module Sidekiq
       Sidekiq.default_job_options["client_class"] = Sidekiq::StagedPush::Client
       Sidekiq::JobUtil::TRANSIENT_ATTRIBUTES << "client_class"
 
-      enqueuer = Enqueuer.new
       Sidekiq.configure_server do |config|
         config.on(:startup) do
+          enqueuer = Enqueuer.new(config)
           enqueuer.start
         end
         config.on(:quiet) do
