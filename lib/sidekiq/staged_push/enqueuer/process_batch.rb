44fe4ffd96baec9b4fb0926e03ba34f377ccc98b
Zulfiqar Ali <desheikh@gmail.com>
2024-12-08 11:35:35 -0500

    transaction block is instantiated only if needed

--- lib/sidekiq/staged_push/enqueuer/process_batch.rb
+++ lib/sidekiq/staged_push/enqueuer/process_batch.rb
@@ -10,19 +10,19 @@ module Sidekiq
         BATCH_SIZE = 500
 
         def call
+          jobs = StagedJob.order(:id).limit(BATCH_SIZE).to_a
+
+          return 0 unless jobs.present?
+
+          client = Sidekiq::Client.new
           StagedJob.transaction do
-            jobs = StagedJob.order(:id).limit(BATCH_SIZE).to_a
-
-            if jobs.present?
-              client = Sidekiq::Client.new
-              jobs.each do |job|
-                client.push(job.payload)
-              end
-              StagedJob.where(id: jobs.map(&:id)).delete_all
+            jobs.each do |job|
+              client.push(job.payload)
             end
-
-            jobs.size
+            StagedJob.where(id: jobs.map(&:id)).delete_all
           end
+
+          jobs.size
         end
       end
     end
